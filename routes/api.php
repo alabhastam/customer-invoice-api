522a3d9f28796eadec91313f02dbd3f54f7d2851
alitorpedo <abdollahia713@gmail.com>
2024-10-08 11:38:19 +0330

    init-controllers-and-routes

---
+++ routes/api.php
@@ -0,0 +1,13 @@
+<?php
+
+use App\Http\Controllers\Api\V1\CustomerController;
+use App\Http\Controllers\Api\V1\InvoiceController;
+use Illuminate\Support\Facades\Route;
+
+Route::prefix('v1')->group(function () {
+    // Customer routes
+    Route::apiResource('customers', CustomerController::class);
+    
+    // Invoice routes
+    Route::apiResource('invoices', InvoiceController::class);
+});
