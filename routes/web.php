6e0ee20253264e7e8a5805f13c3d6d7ca9727032
alitorpedo <abdollahia713@gmail.com>
2024-10-09 13:29:05 +0330

    404-fixed

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
-
+use App\Http\Controllers\Api\V1\CustomerController;
+use App\Http\Controllers\Api\V1\InvoiceController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::prefix('v1')->group(function () {
+    // Customer routes
+    Route::apiResource('customers', CustomerController::class);
+    
+    // Invoice routes
+    Route::apiResource('invoices', InvoiceController::class);
 });
